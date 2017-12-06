6edbc2c3a05b2d41e94ffcd2ef22c157c8445841
LoraMS <lorashopova@abv.bg>
2017-12-06 02:53:02 +0200

    files structure added

---
+++ healthy-recipes/src/environments/environment.ts
@@ -0,0 +1,20 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false
+};
+
+// export const environment = {
+//   production: true,
+//   firebase: {
+//     apiKey: 'AIzaSyA5Xq-80VQGY04-7fY-HRqHLC5rFo5AhM4',
+//     authDomain: 'healthy-recipes-832f5.firebaseapp.com',
+//     databaseURL: 'https://healthy-recipes-832f5.firebaseio.com',
+//     projectId: 'healthy-recipes-832f5',
+//     storageBucket: 'healthy-recipes-832f5.appspot.com',
+//     messagingSenderId: '776903252026'
+//   }
+// };
