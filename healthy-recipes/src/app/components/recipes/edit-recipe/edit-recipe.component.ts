a6dfea0666bab6ceaa76ecfe94ab14d12bc31066
LoraMS <lorashopova@abv.bg>
2017-12-11 21:31:22 +0200

    edit recipe component added

--- healthy-recipes/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
+++ healthy-recipes/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import { Recipe } from '../../../models/recipe';
+import { RecipesService } from './../../../services/recipes.service';
+
+const categories = ['breakfast', 'soups', 'salads', 'desserts', 'breads', 'main dishes', 'side dishes'];
 
 @Component({
   selector: 'app-edit-recipe',
@@ -6,10 +18,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-recipe.component.css']
 })
 export class EditRecipeComponent implements OnInit {
+  public recipeKey: string;
+  public recipe;
 
-  constructor() { }
+  constructor(private data: RecipesService, private router: Router, private toastr: ToastrService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.recipeKey = this.route.snapshot.params['id'];
+    this.data.getRecipeById(this.recipeKey)
+    .valueChanges()
+    .subscribe((result) => {
+      this.recipe = result;
+    });
   }
+
+  editRecipe(formData) {
+    this.data.editRecipe(this.recipeKey, this.recipe);
+    this.toastr.success('Your recipe was updated successfully!', 'Success!');
+
+    this.router.navigate(['recipes']);
+  }
+
+  search = (text$: Observable<string>) =>
+  text$
+    .debounceTime(200)
+    .distinctUntilChanged()
+    .map(term => term.length < 1 ? []
+      : categories.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 5))
 
 }
