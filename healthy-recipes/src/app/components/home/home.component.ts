0798f3d40e3e9867edebfae50c11490f563a0aee
LoraMS <lorashopova@abv.bg>
2017-12-12 06:35:46 +0200

    home component dinamic made

--- healthy-recipes/src/app/components/home/home.component.ts
+++ healthy-recipes/src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from './../../services/articles.service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
+import { Recipe } from './../../models/recipe';
+import { Article } from './../../models/article';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  recipesRef: AngularFireList<Recipe[]> = null;
+  articlesRef: AngularFireList<Article[]> = null;
+  public recipes: any;
+  public articles: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase, private articleData: ArticlesService) {
+    this.recipesRef = db.list('/recipes', ref => ref.limitToLast(1));
+    this.articlesRef = db.list('/articles', ref => ref.limitToLast(1));
+   }
 
   ngOnInit() {
+    this.recipesRef.valueChanges().subscribe(result => {
+      this.recipes = result;
+    });
+
+    this.articlesRef.valueChanges().subscribe(result => {
+      this.articles = result;
+    });
   }
 
 }
